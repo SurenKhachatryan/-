40faaaef978801caa2c1dddaf39b4257c69514f9
Suren Khachatryan < @DESKTOP-76FIPGK>
2018-02-03 20:42:06 +0400

    M.B.N.G.B.T.

---
+++ M.B.N.G.B.T/M.B.N.G.B.T/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace M.B.N.G.B.T
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
