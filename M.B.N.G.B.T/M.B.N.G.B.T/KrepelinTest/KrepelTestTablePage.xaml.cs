9d8e3cc30c7443d8149ddf0bb2c86d790ae32e94
Suren Khachatryan <surench94@gmail.com>
2018-05-04 17:58:48 +0400

    MBNGBT

--- M.B.N.G.B.T/M.B.N.G.B.T/KrepelinTest/KrepelTestTablePage.xaml.cs
+++ M.B.N.G.B.T/M.B.N.G.B.T/KrepelinTest/KrepelTestTablePage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace M.B.N.G.B.T.KrepelTest
 {
@@ -20,22 +21,79 @@ namespace M.B.N.G.B.T.KrepelTest
     /// </summary>
     public partial class KrepelinTestTablePage : Page
     {
+        private Random rnd = new Random();
+        private DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        
+        private int second = 0;
+
         public KrepelinTestTablePage()
         {
             InitializeComponent();
+
+            dispatcherTimer.Tick += new EventHandler(LabelTimer);
+            Initializator();
             textBox.Focus();
+            dispatcherTimer.Start();
+
         }
 
         private void button_Click_Result(object sender, RoutedEventArgs e)
         {
+            dispatcherTimer.Stop();
+        }
 
+        private void LabelTimer(object sender, EventArgs e)
+        {
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            second++;
+            if (textBox.Text != "" && second == 1)
+            {
+                FinishStage();
+            }
         }
 
         private void textBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
+            second = 0;
             if ((!Char.IsDigit(e.Text, 0)) || textBox.Text.Length == 3)
             {
                 e.Handled = true;
+            }
+            //if (Char.IsDigit(e.Text, 0))
+            //{
+            //    textBox.Text = e.Text;
+            //    FinishStage();
+            //    e.Handled = true;
+            //}
+        }
+
+        private void Grid_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            second = 0;
+            if (e.Key == Key.Space)
+            {
+                e.Handled = true;
+            }
+
+        }
+        private void Initializator()
+        {
+            LableCalculatingUp.Content = $"{rnd.Next(1, 10)}";
+            LableCalculatingDown.Content = $"{rnd.Next(1, 10)}";
+            textBox.Text = "";
+        }
+
+        private void FinishStage()
+        {
+            if (Convert.ToInt32(textBox.Text) == ((Convert.ToInt32(LableCalculatingUp.Content) + Convert.ToInt32(LableCalculatingDown.Content)) % 10))
+            {
+                lebleTrue.Content = $"Ճիշտ = {(Convert.ToInt32(lebleTrue.Content) + 1)}";
+                Initializator();
+            }
+            else
+            {
+                lebleFalse.Content = $"Սխալ = {Convert.ToInt32(lebleFalse) + 1}";
+                Initializator();
             }
         }
     }
